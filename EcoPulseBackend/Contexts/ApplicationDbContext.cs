a8786e552e7dfd91289a16500d58da30e7ba1b82
xjasz <novoselov.s@v2grp.ru>
2026-02-03 10:44:54 +0700

    feat: add initial migration

--- EcoPulseBackend/Contexts/ApplicationDbContext.cs
+++ EcoPulseBackend/Contexts/ApplicationDbContext.cs
@@ -1,18 +1,25 @@
+using EcoPulseBackend.Enums;
+using EcoPulseBackend.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace EcoPulseBackend.Contexts;
 
 public class ApplicationDbContext : DbContext
 {
+    public virtual DbSet<PollutantInfo> PollutantInfos { get; set; } = null!;
+    
     public ApplicationDbContext() {  }
     
     public ApplicationDbContext(DbContextOptions options) : base(options)
     {
-        
     }
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
+
+        builder.Entity<PollutantInfo>().HasData(
+            new PollutantInfo { Id = 1, Code = 2, Name = "Твердые частицы", ShortName = "PM2.5", Pollutant = Pollutant.SP, Mass = 15.72f, MaxPermissibleConcentration = 0.5f }
+        );
     }
 }
---
+++ EcoPulseBackend/Migrations/20260203034046_Initial.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace EcoPulseBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PollutantInfos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Code = table.Column<int>(type: "integer", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    ShortName = table.Column<string>(type: "text", nullable: false),
+                    Pollutant = table.Column<int>(type: "integer", nullable: false),
+                    SpecificEmission = table.Column<float>(type: "real", nullable: true),
+                    Mass = table.Column<float>(type: "real", nullable: true),
+                    MaxPermissibleConcentration = table.Column<float>(type: "real", nullable: true),
+                    DailyAverageConcentration = table.Column<float>(type: "real", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PollutantInfos", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "PollutantInfos",
+                columns: new[] { "Id", "Code", "DailyAverageConcentration", "Mass", "MaxPermissibleConcentration", "Name", "Pollutant", "ShortName", "SpecificEmission" },
+                values: new object[] { 1, 2, null, 15.72f, 0.5f, "Твердые частицы", 2, "PM2.5", null });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PollutantInfos");
+        }
+    }
+}
