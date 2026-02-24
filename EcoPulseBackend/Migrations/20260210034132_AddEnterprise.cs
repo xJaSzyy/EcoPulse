8ea5358cbe8f5b3f1b4a8c458a9cb2d2776acd49
xjasz <novoselov.s@v2grp.ru>
2026-02-24 10:07:14 +0700

    fix: add missed migration

---
+++ EcoPulseBackend/Migrations/20260210034132_AddEnterprise.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using NetTopologySuite.Geometries;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace EcoPulseBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEnterprise : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "EnterpriseId",
+                table: "SingleEmissionSources",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Enterprises",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    SanitaryArea = table.Column<Polygon>(type: "geometry", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Enterprises", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SingleEmissionSources_EnterpriseId",
+                table: "SingleEmissionSources",
+                column: "EnterpriseId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SingleEmissionSources_Enterprises_EnterpriseId",
+                table: "SingleEmissionSources",
+                column: "EnterpriseId",
+                principalTable: "Enterprises",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SingleEmissionSources_Enterprises_EnterpriseId",
+                table: "SingleEmissionSources");
+
+            migrationBuilder.DropTable(
+                name: "Enterprises");
+
+            migrationBuilder.DropIndex(
+                name: "IX_SingleEmissionSources_EnterpriseId",
+                table: "SingleEmissionSources");
+
+            migrationBuilder.DropColumn(
+                name: "EnterpriseId",
+                table: "SingleEmissionSources");
+        }
+    }
+}
